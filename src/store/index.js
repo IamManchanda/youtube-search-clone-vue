01cbaa2e35615a50f5056115c35ca12eac3a37a2
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-05-07 22:09:59 +0530

    Add comment why store is empty!

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,9 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+// No State Management needed in this App, so Store is empty!
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
 });
